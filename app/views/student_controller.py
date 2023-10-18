11dab741a03352ac8e1aa0d4bcbb67165116d3bf
lionaq <romeolian123@gmail.com>
2023-10-18 16:29:19 +0800

    table fix

--- app/views/student_controller.py
+++ app/views/student_controller.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from app.Models import studentModel
 
 student = Blueprint('student', __name__)
 
 @student.route('/student')
 def data():
-    return render_template('student.html')
+    result = studentModel.all()
+    print(result)
+    return render_template('student.html', data = result)
