0f072d077805e3d307c07530e74254eb7516a18c
lionaq <romeolian123@gmail.com>
2023-10-20 06:12:23 +0800

    feat: college controller

--- app/views/college_controller.py
+++ app/views/college_controller.py
@@ -3,8 +3,51 @@ from app.Models import collegeModel
 
 college = Blueprint('college', __name__)
 
+
 @college.route('/college')
 def data():
     result = collegeModel.all()
     print(result)
     return render_template('college.html', data = result)
+
+
+@college.route('/college/insert', methods = ['POST'])
+def insert():
+    if request.method == "POST":
+        courseCode = request.form['course_code']
+        course = request.form['course']
+        collegeCode = request.form['college_code']
+
+        list = [courseCode.upper(), course, collegeCode]
+
+        print(list)
+        try:
+            collegeModel.insert(list)
+            return redirect (url_for('college.data', success = True))
+        except: 
+            return redirect (url_for('college.data', error = True))
+
+
+@college.route('/college/update', methods = ['POST'])
+def update():
+    if request.method == "POST":
+
+        college_code = request.form['course_code_edit']
+        course_edit = request.form['course_edit']
+        college_code_edit = request.form['college_code_edit']
+
+        list = [course_edit.title(), college_code_edit, college_code]
+        print(list)
+        try:
+            collegeModel.update(list)
+            return redirect (url_for('college.data', success = True))
+        except: 
+            return redirect (url_for('college.data', error = True))
+        
+
+@college.route('/college/delete/<string:id>', methods=['POST'])
+def delete(id):
+    if request.method == "POST":
+        data = (id,)
+        collegeModel.delete(data)
+        return redirect(url_for('college.data', success=True))
